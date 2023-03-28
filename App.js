8d8705ae8312e8931a32663c26ca9b3c4abc2e46
srianbury <bsunbury29@gmail.com>
2023-03-27 22:15:23 -0700

    refactor

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { FirstScreen } from "./FirstScreen";
 
 export default function App() {
-  return <FirstScreen />;
+  return (
+    <View>
+      <FirstScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
