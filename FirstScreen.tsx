00781b78d2f690fea993943a4655113c7e000c21
srianbury <bsunbury29@gmail.com>
2023-03-27 22:28:52 -0700

    The Basics: Using a ScrollView

--- FirstScreen.tsx
+++ FirstScreen.tsx
@@ -1,24 +1,72 @@
 import { useState } from "react";
-import { Text, View, TextInput } from "react-native";
+import { Image, ScrollView, Text } from "react-native";
+
+type Logo = {
+  uri: string;
+  width: number;
+  height: number;
+};
+
+const logo: Logo = {
+  uri: "https://reactnative.dev/img/tiny_logo.png",
+  width: 64,
+  height: 64,
+};
+
+type TextStyle = {
+  fontSize: number;
+};
+
+const textStyle: TextStyle = {
+  fontSize: 96,
+};
+
+function ReactTinyLogo(): JSX.Element {
+  return <Image source={logo} />;
+}
+
+type LargeTextProps = {
+  children: any;
+};
+function LargeText({ children }: LargeTextProps): JSX.Element {
+  return <Text style={textStyle}>{children}</Text>;
+}
 
 function FirstScreen(): JSX.Element {
-  const [text, setText] = useState<string>("");
-
   return (
-    <View style={{ padding: 10 }}>
-      <TextInput
-        style={{ height: 40, marginTop: 40 }}
-        placeholder="Type here to translate..."
-        onChangeText={setText}
-        defaultValue={text}
-      />
-      <Text style={{ padding: 10, fontSize: 42 }}>
-        {text
-          .split(" ")
-          .map((word) => (word.trim() !== "" ? "🍕" : ""))
-          .join(" ")}
-      </Text>
-    </View>
+    <ScrollView>
+      <LargeText>Scroll me plz</LargeText>
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <LargeText>If you like</LargeText>
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <LargeText>Scrolling down</LargeText>
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <LargeText>What's the best</LargeText>
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <LargeText>Framework around?</LargeText>
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <ReactTinyLogo />
+      <LargeText>React Native</LargeText>
+    </ScrollView>
   );
 }
 
